56ce9880b4ee3b9205e9db119afa2878e0e42975
Elber Nunes <nunes_elber@hotmail.com>
2022-10-01 18:34:24 -0300

    Added user profile feed api

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
+
 from profiles_api import models
 
+
 class HelloSerializer(serializers.Serializer):
-    """Serializes a name field for testing out APIView"""
+    """Serializes a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
 
 
@@ -29,7 +31,6 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
         return user
 
-
     def update(self, instance, validated_data):
         """Handle updating user account"""
         if 'password' in validated_data:
@@ -37,3 +38,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
             instance.set_password(password)
 
         return super().update(instance, validated_data)
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
